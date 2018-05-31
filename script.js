37d751e3e2b9c3877488d412f5e39721e4872978
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-05-31 11:45:01 -0500

    Ya grafica y ordena

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 var canvas = document.getElementById("myCanvas");
 var context = canvas.getContext("2d");
 
+var btnOrder = document.getElementById("btnOrder");
+
+var list = [10, 12, 23, 6, 3, 11, 18, 8];
+
 //Invierte el eje de las x
 context.transform(1, 0, 0, -1, 0, canvas.height);
 
+//Return the max number in an array
 function getMax(arr){
     var max = arr[0];
 
@@ -16,20 +21,48 @@ function getMax(arr){
     return max;
 }
 
+//Draw the rectangles in the canvas
 function drawRectangles(max, len, section){
+    context.beginPath();
+    context.rect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = "white";
+    context.fill();
+    context.closePath();
+
     for(var i = 0; i < list.length; i++){
+        context.beginPath();
         context.rect(section * i + 1, 0, section, list[i] * 5);
         context.fillStyle = "deepskyblue";
         context.strokeStyle = "blue";
         context.stroke();
         context.fill();
+        context.closePath();
     }
 }
-
-var list = [10, 12, 23, 6, 3, 11, 18];
 
 var max = getMax(list);
 var len = list.length;
 var section = canvas.width / len;
 
 drawRectangles(max, len, section);
+
+//Order the array by bubble
+function order(list){
+    for(var i = 0; i < list.length; i++){
+        for(var j = 0; j < list.length; j++) {
+            if(list[i] < list[j]){
+                var temp = list[j];
+                list[j] = list[i];
+                list[i] = temp;
+
+                window.setTimeout(() => {
+                    drawRectangles(max, len, section);                            
+                }, 1000);
+            }
+        }
+    }
+}
+
+btnOrder.addEventListener("click", () => {
+    order(list);
+});
