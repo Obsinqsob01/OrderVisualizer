b244b2b2fcc031a33ac2d516d13453ecf45018b5
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-05-31 11:05:16 -0500

    Grafica el array

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var canvas = document.getElementById("myCanvas");
 var context = canvas.getContext("2d");
 
+//Invierte el eje de las x
+context.transform(1, 0, 0, -1, 0, canvas.height);
+
 function getMax(arr){
     var max = arr[0];
 
@@ -13,9 +16,20 @@ function getMax(arr){
     return max;
 }
 
+function drawRectangles(max, len, section){
+    for(var i = 0; i < list.length; i++){
+        context.rect(section * i + 1, 0, section, list[i] * 5);
+        context.fillStyle = "deepskyblue";
+        context.strokeStyle = "blue";
+        context.stroke();
+        context.fill();
+    }
+}
+
 var list = [10, 12, 23, 6, 3, 11, 18];
 
 var max = getMax(list);
 var len = list.length;
+var section = canvas.width / len;
 
-
+drawRectangles(max, len, section);
