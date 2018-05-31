0bfb0cd74d9b1fdd125b51dd058deece200d4e37
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-05-31 10:42:17 -0500

    Ya da el numero maxico

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 var canvas = document.getElementById("myCanvas");
 var context = canvas.getContext("2d");
 
+function getMax(arr){
+    var max = arr[0];
+
+    for(var i = 0; i < arr.length; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    
+    return max;
+}
+
+var list = [10, 12, 23, 6, 3, 11, 18];
+
+var max = getMax(list);
+var len = list.length;
+
 
